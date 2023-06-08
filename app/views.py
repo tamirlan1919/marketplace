af0e2d53bfe006529a312fe822c53bb8b870be1c
mac <mac@MacBook-Pro-mac.local>
2023-06-08 17:42:37 +0300

    fix verify

--- app/views.py
+++ app/views.py
@@ -15,7 +15,7 @@ from django.template.loader import render_to_string
 from .token import account_activation_token 
 from django.contrib.auth.models import User 
 from django.core.mail import send_mail
-from .models import UserProfile
+from 
 from django.core.mail import EmailMessage 
 # Create your views here.
 
@@ -55,49 +55,42 @@ def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
+            # save form in the memory not in database
             user = form.save(commit=False)
             user.is_active = False
             user.save()
 
-            user_profile = UserProfile.objects.create()
-            user_profile.user = user
-            user_profile.save()
-
+            # Get activation link
             current_site = get_current_site(request)
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
+            token = account_activation_token.make_token(user)
+            activation_link = f"http://{current_site.domain}/activate/{uid}/{token}/"
+
+            # Create email subject and message
             mail_subject = 'Activation link has been sent to your email id'
-            message = render_to_string('acc_active_email.html', {
-                'user': user,
-                'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': account_activation_token.make_token(user),
-            })
-            to_email = form.cleaned_data.get('email')
-            email = EmailMessage(
-                mail_subject, message, to=[to_email]
-            )
-            email.send()
+            message = f"Hello {user.username},\n\nClick the following link to activate your account:\n\n{activation_link}"
+
+            # Send email
+            send_mail(mail_subject, message, 'tchinchaev@bk.ru', [user.email])
 
             return HttpResponse('Please confirm your email address to complete the registration')
     else:
         form = SignupForm()
-
     return render(request, 'register.html', {'form': form})
 
-
-def activate(request, uidb64, token):
-    User = get_user_model()
-    try:
-        uid = force_str(urlsafe_base64_decode(uidb64))
-        user = User.objects.get(pk=uid)
-    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-        user = None
-
-    if user is not None and account_activation_token.check_token(user, token):
-        user.is_active = True
-        user.save()
-        return HttpResponse('Thank you for your email confirmation. Now you can log in to your account.')
-    else:
-        return HttpResponse('Activation link is invalid!')
+def activate(request, uidb64, token): 
+    User = get_user_model() 
+    try: 
+        uid = force_str(urlsafe_base64_decode(uidb64)) 
+        user = User.objects.get(pk=uid) 
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist): 
+        user = None 
+    if user is not None and account_activation_token.check_token(user, token): 
+        user.is_active = True 
+        user.save() 
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.') 
+    else: 
+        return HttpResponse('Activation link is invalid!') 
 
 def user_logout(request):
     logout(request)
